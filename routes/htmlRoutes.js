2a4a0baa20b87e323fd81a605651bb92e5b18ed6
Matthew Romano <mathiasrok@gmail.com>
2021-03-06 19:48:42 -0500

    restructured html routes again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,14 @@
+const { createHash } = require('crypto');
 const express = require('express');
 const path = require('path');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    try {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    } catch (err) {
+        res.status(500).end();
+    }
 });
 
 router.get("/notes", (req, res) => {
