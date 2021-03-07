d64611086d76b4d1bc00d8a2828bcb356a5b2b98
Matthew Romano <mathiasrok@gmail.com>
2021-03-06 19:47:37 -0500

    restructured html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,11 +7,19 @@ router.get("/", (req, res) => {
 });
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    try { 
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    } catch (err) {
+        res.status(500).end();
+    }
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    try {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    } catch (err) {
+        res.status(500).end();
+    }
 });
 
 module.exports = router;
